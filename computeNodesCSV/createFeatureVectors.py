4aa920d20732800af043d38665f1457759ae18d0
DarthVi <covinc93@gmail.com>
2020-06-30 15:37:06 +0200

    added training and classification with 60-40 split

--- computeNodesCSV/createFeatureVectors.py
+++ computeNodesCSV/createFeatureVectors.py
@@ -10,6 +10,13 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import argparse
+import multiprocessing
+import itertools
+
+"""grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"""
+def grouper(n, iterable, fillvalue=None):
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 def getMostRecentFault(arr):
     #if everything is labeled with "healthy" (label 0), the sum is 0, return 0
@@ -29,21 +36,49 @@ the sum of the changes within the aggregation window during rolling window opera
 def getSumChanges(arr):
     return np.sum(np.diff(arr))
 
+#orig_df = df[column]
+#for each column, calculate the indicators defined in the LRZ report of March 2020
+def calculateIndicators(column, orig_df, window, step, numfeatures):
+    new_df = pd.DataFrame(index=orig_df.index)
+    new_df['placeholder'] = 1
+    new_df = new_df.rolling(window).mean()[::step]
+    new_df.drop('placeholder', axis=1, inplace=True)
+    if column != None:
+        if column != 'label':
+            new_df['mean_' + column] = orig_df.rolling(window).mean()[::step]
+            new_df['std_' +  column] = orig_df.rolling(window).std()[::step]
+            if(numfeatures == 11):
+                new_df['perc5_' + column] = orig_df.rolling(window).quantile(0.05)[::step]
+            new_df['perc25_' + column] = orig_df.rolling(window).quantile(0.25)[::step]
+            new_df['perc75_' + column] = orig_df.rolling(window).quantile(0.75)[::step]
+            if(numfeatures == 11):
+                new_df['perc95_' + column] = orig_df.rolling(window).quantile(0.95)[::step]
+            new_df['sumdiff_' +  column] = orig_df.rolling(window).agg(getSumChanges)[::step]
+            new_df['last_' + column] = orig_df.rolling(window).agg(lambda x: np.asarray(x)[-1])[::step]
+            if(numfeatures == 11):
+                new_df['min_' + column] = orig_df.rolling(window).min()[::step]
+                new_df['max_' + column] = orig_df.rolling(window).max()[::step]
+                new_df['median_' + column] = orig_df.rolling(window).median()[::step]
+        else:
+            new_df['label'] = orig_df.rolling(window).agg(getMostRecentFault)[::step]
+
+    return new_df
+
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--timewindow", type=str, default='60s') 
+    parser.add_argument("-w", "--timewindow", type=int, default=60) 
     parser.add_argument("-f", "--features", type=int, default=6)
     parser.add_argument("-s", "--stepsize", type=int, default=1)
+    parser.add_argument("-p", "--processes", type=int, default=4)
     args = parser.parse_args()
 
     if args.features != 6 and args.features != 11:
         print("Wrong arguments for features")
         exit(1)
 
-    timewindow = args.timewindow
-    num_timewindow = int(timewindow[:-1])
+    num_timewindow = args.timewindow
 
     here = pathlib.Path(__file__).parent
 
@@ -66,25 +101,23 @@ if __name__ == '__main__':
             new_df = new_df.rolling(num_timewindow).mean()[::args.stepsize]
             new_df.drop('placeholder', axis=1, inplace=True)
 
-            #for each column, calculate the indicators defined in the LRZ report of March 2020
-            for column in tqdm(orig_df.columns):
-                if column != 'label':
-                    new_df['mean_' + column] = orig_df[column].rolling(num_timewindow).mean()[::args.stepsize]
-                    new_df['std_' +  column] = orig_df[column].rolling(num_timewindow).std()[::args.stepsize]
-                    if(args.features == 11):
-                        new_df['perc5_' + column] = orig_df[column].rolling(num_timewindow).quantile(0.05)[::args.stepsize]
-                    new_df['perc25_' + column] = orig_df[column].rolling(num_timewindow).quantile(0.25)[::args.stepsize]
-                    new_df['perc75_' + column] = orig_df[column].rolling(num_timewindow).quantile(0.75)[::args.stepsize]
-                    if(args.features == 11):
-                        new_df['perc95_' + column] = orig_df[column].rolling(num_timewindow).quantile(0.95)[::args.stepsize]
-                    new_df['sumdiff_' +  column] = orig_df[column].rolling(num_timewindow).agg(getSumChanges)[::args.stepsize]
-                    new_df['last_' + column] = orig_df[column].rolling(num_timewindow).agg(lambda x: np.asarray(x)[-1])[::args.stepsize]
-                    if(args.features == 11):
-                        new_df['min_' + column] = orig_df[column].rolling(num_timewindow).min()[::args.stepsize]
-                        new_df['max_' + column] = orig_df[column].rolling(num_timewindow).max()[::args.stepsize]
-                        new_df['median_' + column] = orig_df[column].rolling(num_timewindow).median()[::args.stepsize]
-                else:
-                    new_df['label'] = orig_df[column].rolling(num_timewindow).agg(getMostRecentFault)[::args.stepsize]
+            #For each column, calculate the indicators defined in the LRZ report of March 2020.
+            #This is done using multiprocessing, each process addresses a column.
+            #First we group columns in group of n (where n is the number of processes)
+            for groupcol in tqdm(list(grouper(args.processes, orig_df.columns))):
+                #create a list of tuples, where each tuple is made by (column_name, df[column], ...otherarguments)
+                arg_list = []
+                for elem in groupcol:
+                    if elem != None:
+                        arg_list.append((elem, orig_df[elem], num_timewindow, args.stepsize, args.features))
+                with multiprocessing.Pool(processes=args.processes) as pool:
+                    results = pool.starmap(calculateIndicators, arg_list)
+                for dataframe in results:
+                    if dataframe.empty == False:
+                        new_df = pd.concat((new_df, dataframe), axis=1)
+                
+            #reorder columns lexycographically
+            #new_df = new_df.reindex(sorted(new_df.columns), axis=1)
 
             #put label as last column
             label_col = new_df.pop('label')
