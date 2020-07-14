e71d3f9f35ed61eed9581da4245d6ea79b8609c8
DarthVi <covinc93@gmail.com>
2020-07-14 14:53:19 +0200

    added forking and multiprocessing parameter to splitdata.sh

--- splitdata.sh
+++ splitdata.sh
@@ -2,10 +2,25 @@
 
 source ~/DCDB/install/dcdb.bash
 
+function query_and_store
+{
+	LD_PRELOAD=./isatty.so dcdbquery -r -h 127.0.0.1 $1 $2 $3 | tail -n +3 > $4
+}
+
+i=0
 while read p; do
+	i=$((i+1))
 	#echo "$p"
 	outputname=${p////}
 	echo "$outputname"
 	output="$outputname.csv"
-	LD_PRELOAD=./isatty.so dcdbquery -r -h 127.0.0.1 $p $1 $2 | tail -n +3 >> $output 
+	#execute it in background in parallel
+	query_and_store $p $1 $2 $output &
+	#if we have launched $3 tasks, wait for them to finish before starting next batch
+	if ! ((i % $3)); then
+		wait
+	fi
 done <sensorlist_data.txt
+
+wait
+echo "All CSVs have been retrieved"
