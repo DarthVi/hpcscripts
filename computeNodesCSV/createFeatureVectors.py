ba690c38d2a0ec89df7b55a5cf2508abbf7ab9c8
DarthVi <covinc93@gmail.com>
2020-08-04 17:03:52 +0200

    Creation of feature vectors now is done in chunks. Added correction to experiment 3

--- computeNodesCSV/createFeatureVectors.py
+++ computeNodesCSV/createFeatureVectors.py
@@ -13,6 +13,19 @@ import argparse
 import multiprocessing
 import itertools
 
+#######functions to count lines of a file, from https://stackoverflow.com/a/27518377/1262118
+def _make_gen(reader):
+    b = reader(1024 * 1024)
+    while b:
+        yield b
+        b = reader(1024*1024)
+
+def rawgencount(filename):
+    f = open(filename, 'rb')
+    f_gen = _make_gen(f.raw.read)
+    return sum( buf.count(b'\n') for buf in f_gen )
+########
+
 """grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"""
 def grouper(n, iterable, fillvalue=None):
     args = [iter(iterable)] * n
@@ -64,6 +77,20 @@ def calculateIndicators(column, orig_df, window, step, numfeatures):
 
     return new_df
 
+"""
+Calculate the next index from which to start in the next iteration of the algorithm
+"""
+def calculateNextIndex(current_index, numchunk, numwindow, stepsize):
+    tmp_line =  numchunk
+
+    count = 0
+    while(tmp_line >= numwindow):
+        tmp_line = tmp_line - stepsize
+        count = count + 1
+
+    return (current_index + count*stepsize, count*stepsize)
+
+
 
 if __name__ == '__main__':
 
@@ -72,6 +99,7 @@ if __name__ == '__main__':
     parser.add_argument("-f", "--features", type=int, default=6)
     parser.add_argument("-s", "--stepsize", type=int, default=1)
     parser.add_argument("-p", "--processes", type=int, default=4)
+    parser.add_argument("-c", "--chunk", type=int, default=10000)
     args = parser.parse_args()
 
     if args.features != 6 and args.features != 11:
@@ -86,43 +114,61 @@ if __name__ == '__main__':
         if file_entry.is_file() and file_entry.suffix == '.csv' and '_' not in file_entry.name:
             filepath = here.joinpath(file_entry.name)
             print("Processing file " + file_entry.name)
-            orig_df = pd.read_csv(filepath, header=0, index_col=0, parse_dates=True)
-            #remove columns with name "cpuXX/<metricname>"
-            orig_df = orig_df.loc[:, ~orig_df.columns.str.contains('cpu[0-9]')]
-            #drop applicationLabel and faultPred
-            orig_df.drop(['experiment/applicationLabel', 'faultPred'], axis=1, inplace=True)
-            #rename faultLabel in label
-            orig_df.rename(columns={'faultLabel' : 'label'}, inplace=True)
-            orig_df.reset_index(drop=True, inplace=True)
-            #initialize new dataframe to empty timeseries
-            new_df = pd.DataFrame(index=orig_df.index)
-            new_df['placeholder'] = 1
-            #group by with a window of 60s, the column placeholder is just a trick to make groupby work, we can remove it afterwards
-            new_df = new_df.rolling(num_timewindow).mean().dropna()[::args.stepsize].reset_index(drop=True)
-            new_df.drop('placeholder', axis=1, inplace=True)
+            #get line count by counting how many newlines there are
+            orig_numlines = rawgencount(filepath)
+            #get row count, excluding the header
+            orig_numlines = orig_numlines - 1
+            print("File lines: ", orig_numlines)
 
-            #For each column, calculate the indicators defined in the LRZ report of March 2020.
-            #This is done using multiprocessing, each process addresses a column.
-            #First we group columns in group of n (where n is the number of processes)
-            for groupcol in tqdm(list(grouper(args.processes, orig_df.columns))):
-                #create a list of tuples, where each tuple is made by (column_name, df[column], ...otherarguments)
-                arg_list = []
-                for elem in groupcol:
-                    if elem != None:
-                        arg_list.append((elem, orig_df[elem], num_timewindow, args.stepsize, args.features))
-                with multiprocessing.Pool(processes=args.processes) as pool:
-                    results = pool.starmap(calculateIndicators, arg_list)
-                for dataframe in results:
-                    if dataframe.empty == False:
-                        new_df = pd.concat((new_df, dataframe), axis=1)
-                
-            #reorder columns lexycographically
-            #new_df = new_df.reindex(sorted(new_df.columns), axis=1)
+            isFirst = True
 
-            #put label as last column
-            label_col = new_df.pop('label')
-            new_df['label'] = label_col
+            if os.path.exists(here.joinpath(file_entry.stem + f"_{args.features}f_{num_timewindow}s_{args.stepsize}step.csv")):
+                os.remove(here.joinpath(file_entry.stem + f"_{args.features}f_{num_timewindow}s_{args.stepsize}step.csv"))
 
-            print("Saving feature vectors for node " + file_entry.stem)
-            new_df.to_csv(here.joinpath(file_entry.stem + f"_{args.features}f_{num_timewindow}s_{args.stepsize}step.csv"), index=False)
-            print("Saving done")
+            pbar = tqdm(total=orig_numlines - num_timewindow + 1)
+            currindex = 1
+            while(currindex < orig_numlines):
+                orig_df = pd.read_csv(filepath, header=0, index_col=0, parse_dates=True, skiprows=range(1,currindex), nrows=args.chunk)
+                #remove columns with name "cpuXX/<metricname>"
+                orig_df = orig_df.loc[:, ~orig_df.columns.str.contains('cpu[0-9]')]
+                #drop applicationLabel and faultPred
+                orig_df.drop(['experiment/applicationLabel', 'faultPred'], axis=1, inplace=True)
+                #rename faultLabel in label
+                orig_df.rename(columns={'faultLabel' : 'label'}, inplace=True)
+                orig_df.reset_index(drop=True, inplace=True)
+                #initialize new dataframe to empty timeseries
+                new_df = pd.DataFrame(index=orig_df.index)
+                new_df['placeholder'] = 1
+                #group by with a window of 60s, the column placeholder is just a trick to make groupby work, we can remove it afterwards
+                new_df = new_df.rolling(num_timewindow).mean().dropna()[::args.stepsize].reset_index(drop=True)
+                new_df.drop('placeholder', axis=1, inplace=True)
+
+                #For each column, calculate the indicators defined in the LRZ report of March 2020.
+                #This is done using multiprocessing, each process addresses a column.
+                #First we group columns in group of n (where n is the number of processes)
+                for groupcol in list(grouper(args.processes, orig_df.columns)):
+                    #create a list of tuples, where each tuple is made by (column_name, df[column], ...otherarguments)
+                    arg_list = []
+                    for elem in groupcol:
+                        if elem != None:
+                            arg_list.append((elem, orig_df[elem], num_timewindow, args.stepsize, args.features))
+                    with multiprocessing.Pool(processes=args.processes) as pool:
+                        results = pool.starmap(calculateIndicators, arg_list)
+                    results.insert(0, new_df)
+                    new_df = pd.concat(results, axis=1)
+                    results = []
+
+                    
+                #reorder columns lexycographically
+                #new_df = new_df.reindex(sorted(new_df.columns), axis=1)
+
+                #put label as last column
+                label_col = new_df.pop('label')
+                new_df['label'] = label_col
+
+                new_df.to_csv(here.joinpath(file_entry.stem + f"_{args.features}f_{num_timewindow}s_{args.stepsize}step.csv"), mode='a', index=False, header=isFirst)
+                isFirst = False
+
+                currindex, counter = calculateNextIndex(currindex, args.chunk, num_timewindow, args.stepsize)
+                pbar.update(counter)
+            pbar.close()
