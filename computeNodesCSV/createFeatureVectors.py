5d91223a7edf7def26a5d4ea0ef437284d2b5f9b
DarthVi <covinc93@gmail.com>
2020-08-08 16:17:15 +0200

    Added correlations in feature vectors creation; added code with dask framework; added experiment 11 results

--- computeNodesCSV/createFeatureVectors.py
+++ computeNodesCSV/createFeatureVectors.py
@@ -42,6 +42,18 @@ def getMostRecentFault(arr):
                 currentLabel = label
         return currentLabel
 
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 """
 Get the sum of the changes within the array-like object; used to get
 the sum of the changes within the aggregation window during rolling window operations.
@@ -50,30 +62,39 @@ def getSumChanges(arr):
     return np.sum(np.diff(arr))
 
 #orig_df = df[column]
-#for each column, calculate the indicators defined in the LRZ report of March 2020
-def calculateIndicators(column, orig_df, window, step, numfeatures):
+#for each column, calculate the indicators defined in the LRZ report of March 2020 and the correlations between the columns
+def calculateIndicators(column, orig_df, corrcolumns, window, step, numfeatures, compute_correlations):
     new_df = pd.DataFrame(index=orig_df.index)
     new_df['placeholder'] = 1
     new_df = new_df.rolling(window).mean().dropna()[::step].reset_index(drop=True)
     new_df.drop('placeholder', axis=1, inplace=True)
     if column != None:
         if column != 'label':
-            new_df['mean_' + column] = orig_df.rolling(window).mean().dropna()[::step].reset_index(drop=True)
-            new_df['std_' +  column] = orig_df.rolling(window).std().dropna()[::step].reset_index(drop=True)
+            new_df['mean_' + column] = orig_df[column].rolling(window).mean().dropna()[::step].reset_index(drop=True)
+            new_df['std_' +  column] = orig_df[column].rolling(window).apply(lambda x: np.std(x, ddof=1)).dropna()[::step].reset_index(drop=True)
             if(numfeatures == 11):
-                new_df['perc5_' + column] = orig_df.rolling(window).quantile(0.05).dropna()[::step].reset_index(drop=True)
-            new_df['perc25_' + column] = orig_df.rolling(window).quantile(0.25).dropna()[::step].reset_index(drop=True)
-            new_df['perc75_' + column] = orig_df.rolling(window).quantile(0.75).dropna()[::step].reset_index(drop=True)
+                new_df['perc5_' + column] = orig_df[column].rolling(window).quantile(0.05).dropna()[::step].reset_index(drop=True)
+            new_df['perc25_' + column] = orig_df[column].rolling(window).quantile(0.25).dropna()[::step].reset_index(drop=True)
+            new_df['perc75_' + column] = orig_df[column].rolling(window).quantile(0.75).dropna()[::step].reset_index(drop=True)
             if(numfeatures == 11):
-                new_df['perc95_' + column] = orig_df.rolling(window).quantile(0.95).dropna()[::step].reset_index(drop=True)
-            new_df['sumdiff_' +  column] = orig_df.rolling(window).agg(getSumChanges).dropna()[::step].reset_index(drop=True)
-            new_df['last_' + column] = orig_df.rolling(window).agg(lambda x: np.asarray(x)[-1]).dropna()[::step].reset_index(drop=True)
+                new_df['perc95_' + column] = orig_df[column].rolling(window).quantile(0.95).dropna()[::step].reset_index(drop=True)
+            new_df['sumdiff_' +  column] = orig_df[column].rolling(window).agg(getSumChanges).dropna()[::step].reset_index(drop=True)
+            new_df['last_' + column] = orig_df[column].rolling(window).agg(lambda x: np.asarray(x)[-1]).dropna()[::step].reset_index(drop=True)
             if(numfeatures == 11):
-                new_df['min_' + column] = orig_df.rolling(window).min().dropna()[::step].reset_index(drop=True)
-                new_df['max_' + column] = orig_df.rolling(window).max().dropna()[::step].reset_index(drop=True)
-                new_df['median_' + column] = orig_df.rolling(window).median().dropna()[::step].reset_index(drop=True)
+                new_df['min_' + column] = orig_df[column].rolling(window).min().dropna()[::step].reset_index(drop=True)
+                new_df['max_' + column] = orig_df[column].rolling(window).max().dropna()[::step].reset_index(drop=True)
+                new_df['median_' + column] = orig_df[column].rolling(window).median().dropna()[::step].reset_index(drop=True)
         else:
-            new_df['label'] = orig_df.rolling(window).agg(getMostRecentFault).dropna()[::step].reset_index(drop=True)
+            new_df['label'] = orig_df[column].rolling(window).agg(getMostRecentFault).dropna()[::step].reset_index(drop=True)
+
+        if compute_correlations == True:
+            for othercol in orig_df.columns:
+                if othercol != column and str('corr_' + column + '_' + othercol) in corrcols:
+                    correlation_column_other = orig_df[column].rolling(window).corr(orig_df[othercol]).iloc[window-1:][::step].reset_index(drop=True)
+                    correlation_column_other.fillna(0, inplace=True)
+                    correlation_column_other = correlation_column_other.replace([np.inf], +1)
+                    correlation_column_other = correlation_column_other.replace([-np.inf], -1)
+                    new_df['corr_' + column + '_' + othercol] = correlation_column_other
 
     return new_df
 
@@ -90,6 +111,18 @@ def calculateNextIndex(current_index, numchunk, numwindow, stepsize):
 
     return (current_index + count*stepsize, count*stepsize)
 
+"""
+Set the list of correlation columns names, leaving label out of it
+"""
+def setNamesOfCorrelationColumns(lst, columns):
+    if not lst:
+        for col1 in columns:
+            for col2 in columns:
+                #check that correlations with the label are not in the list, we are not supposed to have label data in the test set;
+                #moreover only one direction is allowed (if there's corr_col1_col2, there's no need to add corr_col2_col1)
+                if col1 != col2 and col1 != 'label' and col2 != 'label' and ('corr_' + col2 + '_' + col1) not in lst:
+                    lst.append('corr_' + col1 + '_' + col2)
+
 
 
 if __name__ == '__main__':
@@ -100,6 +133,7 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--stepsize", type=int, default=1)
     parser.add_argument("-p", "--processes", type=int, default=4)
     parser.add_argument("-c", "--chunk", type=int, default=10000)
+    parser.add_argument("-r", "--corr", type=str2bool, nargs='?', const=True, default=False)
     args = parser.parse_args()
 
     if args.features != 6 and args.features != 11:
@@ -127,6 +161,7 @@ if __name__ == '__main__':
 
             pbar = tqdm(total=orig_numlines - num_timewindow + 1)
             currindex = 1
+            corrcols = []
             while(currindex < orig_numlines):
                 orig_df = pd.read_csv(filepath, header=0, index_col=0, parse_dates=True, skiprows=range(1,currindex), nrows=args.chunk)
                 #remove columns with name "cpuXX/<metricname>"
@@ -136,6 +171,10 @@ if __name__ == '__main__':
                 #rename faultLabel in label
                 orig_df.rename(columns={'faultLabel' : 'label'}, inplace=True)
                 orig_df.reset_index(drop=True, inplace=True)
+
+                #set corrcols list only once, when the list is empty
+                setNamesOfCorrelationColumns(corrcols, orig_df.columns)
+
                 #initialize new dataframe to empty timeseries
                 new_df = pd.DataFrame(index=orig_df.index)
                 new_df['placeholder'] = 1
@@ -151,7 +190,7 @@ if __name__ == '__main__':
                     arg_list = []
                     for elem in groupcol:
                         if elem != None:
-                            arg_list.append((elem, orig_df[elem], num_timewindow, args.stepsize, args.features))
+                            arg_list.append((elem, orig_df, corrcols, num_timewindow, args.stepsize, args.features, args.corr))
                     with multiprocessing.Pool(processes=args.processes) as pool:
                         results = pool.starmap(calculateIndicators, arg_list)
                     results.insert(0, new_df)
---
+++ computeNodesCSV/dask_RFE_featureSelection.py
@@ -0,0 +1,154 @@
+"""
+2020-07-06 10:43
+
+@author: Vito Vincenzo Covella
+"""
+
+import numpy as np
+import dask.dataframe as dd
+import dask.array as da
+import pandas as pd
+from numpy import random
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
+from sklearn.metrics import f1_score
+import sys
+import matplotlib.pyplot as plt
+import os
+
+
+#######functions to count lines of a file, from https://stackoverflow.com/a/27518377/1262118
+def _make_gen(reader):
+    b = reader(1024 * 1024)
+    while b:
+        yield b
+        b = reader(1024*1024)
+
+def rawgencount(filename):
+    f = open(filename, 'rb')
+    f_gen = _make_gen(f.raw.read)
+    return sum( buf.count(b'\n') for buf in f_gen )
+########
+
+def plot_bar_x(measureType, key, value):
+    # this is for plotting purpose
+    index = np.arange(len(key))
+    plt.figure()
+    for i, v in enumerate(value):
+        v = np.round(v,2)
+        color = 'red'
+        if v < 0.8:
+            color = 'chocolate'
+        if v >=0.8:
+            color = 'olive'
+        if v >= 0.89:
+            color = 'olivedrab'
+        if v > 0.9:
+            color = 'limegreen'
+        
+        if i == 0:
+            plt.bar(index[i], v, width = 0.9, edgecolor = 'black', ls = '-', lw = 1.5, color = color)
+            plt.text(index[i] - 0.1, 0.3, str('%.2f'%v), fontsize = 12, rotation = 90)
+        else:
+            plt.bar(index[i], v, width = 0.9, edgecolor = 'black', ls = '--', lw = 0.5, color = color)
+            plt.text(index[i] - 0.1, 0.3, str('%.2f'%v), fontsize = 12, rotation = 90)
+#    plt.xlabel('Genre', fontsize=5)
+    plt.ylabel('F-score', fontsize=20)
+    plt.ylim([0.0,1.0])
+    plt.xticks(index, key, fontsize=15, rotation=270)
+    #plt.title('%s'%measureType)
+    fig1 = plt.gcf()
+    plt.show()
+    plt.draw()
+    fig1.savefig('%s.png'%(measureType), bbox_inches='tight')
+
+if __name__ == '__main__':
+    random.seed(42)
+
+    if len(sys.argv) != 3:
+        print("Please insert a file path to analyze as argument and the number of important features to select")
+        exit(1)
+
+    numImportantFeatures = int(str(sys.argv[2]))
+
+    input_file = str(sys.argv[1])
+    input_name = input_file.split('.')[0]
+    nodename = input_file.split('_')[0].split('/')[-1]
+    fileN_mostImportant = input_name + "_result_RFE_" + str(numImportantFeatures) + "mostImportant.txt"
+    featureFile = input_name + "_RFE_mostImportantFeatures.txt"
+
+    len_features = rawgencount(input_file)
+
+    data = dd.read_csv(input_file, header=0)
+    #all the columns except the label one
+    metricKeys = list(data.columns)[:-1]
+
+    #select the features (all the column except the label one)
+    features = data[list(data.columns)[:-1]]
+    features = features.to_dask_array(lengths=True)
+    #select the labels
+    labels = data[list(data.columns)[-1]]
+    labels = labels.to_dask_array(lengths=True)
+    #60-40 train-test split
+    numTrain = int(0.6*len_features)
+    trainData = features[:numTrain]
+    trainLbl = labels[:numTrain].compute()
+    testData = features[numTrain:]
+    testLbl = labels[numTrain:].compute()
+
+    clf = RandomForestClassifier(n_estimators=30, max_depth=20, n_jobs=-1, random_state=42)
+
+    #initialize RFE with RF classifier and numImportantFeatures to select
+    rfe = RFE(clf, n_features_to_select=numImportantFeatures)
+
+    X_rfe = rfe.fit_transform(trainData, trainLbl)
+
+    #Fitting the data to model
+    clf.fit(X_rfe, trainLbl)
+    temp = pd.Series(rfe.support_,index = metricKeys)
+    selected_features_rfe = temp[temp==True].index
+    print("Selected features with RFE:")
+    print(selected_features_rfe)
+
+    with open(featureFile, 'w') as out:
+        out.write('- Most important features:\n')
+
+    #save most important columns in a text file
+    with open(featureFile, 'a') as out:
+        for col in selected_features_rfe:
+            out.write('---- %s\n' % col)
+
+    #select new data using only the most important columns
+    features = data[selected_features_rfe.tolist()]
+    features = features.to_dask_array(lengths=True)
+    #labels are the same as before (see above)
+    trainData = features[:numTrain]
+    #trainLbl are the same as before, no need to recompute them
+    testData = features[numTrain:]
+    #testLbl are the same as before, no need to recompute them
+
+    F = []
+
+    with open(fileN_mostImportant, 'w') as out:
+        out.write('- Classifier: %s\n' % clf.__class__.__name__)
+
+    clf.fit(trainData, trainLbl)
+    pred = clf.predict(testData)
+    print('- Classifier: %s' % clf.__class__.__name__)
+    f1 = f1_score(testLbl, pred, average = 'weighted')
+    F.append(f1)
+
+    for l in np.unique(np.asarray(testLbl)):
+        #select the correct labels from testLbl
+        lab_tmp = testLbl[list(np.where(testLbl == l)[0])]
+        #select the corresponding predicted lables
+        pred_tmp = pred[list(np.where(testLbl == l)[0])]
+        f1 = f1_score(lab_tmp, pred_tmp, average = 'micro')
+        F.append(f1)
+        print('Fault: %d,  F1: %f.\n'%(l,f1))
+        with open(fileN_mostImportant, 'a') as out:
+            out.write('Fault: %d,  F1: %f.\n'%(l,f1))
+
+    keys = ['overall','healthy', 'memeater','memleak', 'membw', 'cpuoccupy','cachecopy','iometadata','iobandwidth']
+    measureType = input_name + "_result_RFE" + str(numImportantFeatures) + "mostImportant"
+    plot_bar_x(measureType, keys, F)
